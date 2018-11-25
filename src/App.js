aa693783712f2dd113f3266f08fbb61ed8641211
Isabelle Zhou <isabellezhou@Isabelles-MacBook-Pro.local>
2018-11-24 21:27:21 -0500

    basic styling of list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import home from './home.png';
 import './App.css';
 import List from './components/List/List';
 
@@ -7,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <img src={home} className="logo" alt="Home Logo" height="42" width="42"></img>
         <List />
       </div>
     );
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -3,19 +3,60 @@ import Item from '../Item/Item';
 import './List.css';
 
 const ingredients= ['apple', 'orange', 'pear', 'bread'];
+const enterKey = 13;
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            items: ingredients
+            items: ingredients,
+            currItem: ''
         };
+        this.handleItem = this.handleItem.bind(this);
+        this.addItem = this.addItem.bind(this);
+    }
+
+    handleItem(event) {
+        console.log('getting here');
+        const { currItem} = this.state;
+        this.setState({
+            currItem: event.target.value
+        });
+        console.log(currItem);
+    }
+
+    addItem(e){
+     const { currItem, items } = this.state;
+     if (e && e.charCode === enterKey) {
+        if (currItem !== '') {
+          this.setState({
+            currItem: ''
+          });
+          if (currItem.indexOf(',') !== 0) {
+            this.setState({
+              items: items.concat(currItem.split(','))
+            });
+          } else {
+            this.setState({
+              items: items.concat(currItem)
+            });
+          }
         }
+ 
+     }
+    }
+    
     render() {
-        const { items }= this.state;
+        const { items, handleItem, addItem }= this.state;
         return (
         <div className="App">
-            <h1>Grocery List for Week 7/10-7/16</h1>
+            <h1 className="date">Grocery List for Week 7/10-7/16</h1>
+            <input
+                  onKeyPress={addItem}
+                  className="inputText"
+                  placeholder="Add item"
+                  onChange={handleItem}
+                />
             <ul className="display">
                     {items.map(item => (
                     <Item text={item} />

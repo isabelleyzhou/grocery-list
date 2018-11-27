edfbb6863abbb5432db2529cd308df21e64c4d1f
Kjue <kelvinjue@gmail.com>
2018-11-26 23:41:26 -0800

    styling v1

--- src/components/Dashboard/Date/Date.jsx
+++ src/components/Dashboard/Date/Date.jsx
@@ -1,20 +1,25 @@
-import React, { Component } from 'react';
-import './Date.css';
-import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import "./Date.css";
+import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
 
 class Date extends Component {
   static propTypes = {
     color: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired
   };
 
   render() {
-    const { title} = this.props;
+    const { title } = this.props;
+    const date = title.split(" ");
+    console.log(date);
+    const month = date[0],
+      number = date[1];
     return (
       <NavLink className="date-box" to="/list">
-        <div className= 'aqua'/>
-        <p className="title-p">{title}</p>
+        <div className="header"> {month} </div>
+        <div className="aqua" />
+        <p className="title-p">{number}</p>
       </NavLink>
     );
   }
